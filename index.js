12c7dcb4558ff6ed9cea24517a66bf167b08d164
Xiaohu Zheng <zxh635148320@gmail.com>
2022-09-04 02:49:53 -0400

    Add database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://xiaohu:1228@cluster0.hhkzd3t.mongodb.net/?retryWrites=true&w=majority")
+
 const cors = require('cors')
 app.use(cors())
 
